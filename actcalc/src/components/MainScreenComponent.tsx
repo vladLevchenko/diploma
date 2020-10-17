8a1a26046899053d879f47e996a952734d20dac1
vlad.levchenko <volodymyr.levchenko@itera.no>
2020-10-17 22:26:42 +0300

    added component

---
+++ actcalc/src/components/MainScreenComponent.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import React from "react";
+
+interface Props {}
+interface State {}
+
+export class MainScreenComponent extends Component<State, Props>{
+    render(){
+        return(<div></div>)
+    }
+}
